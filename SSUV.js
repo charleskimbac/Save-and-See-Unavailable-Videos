0b06d6f79ad511b5faeab03dc4e7588877f66e6a
charles k <52868068+charleskimbac@users.noreply.github.com>
2023-06-22 22:56:07 -0400

    left section improved, ui+, text/code fixes

--- SSUV.js
+++ SSUV.js
@@ -1,40 +1,35 @@
 import { saveData, youtubeKey, getData } from "./secret.js";
 
-document.getElementById("current-videos").style.display = "none";
+document.getElementById("show-data").style.display = "none";
 document.getElementById("invalid-id").style.display = "none"; 
 
-
-function playlistEntered(){
-    console.log("pressed enter");
-    var playlistId = document.getElementById("playlistId").value;
-    checkPlaylistId(playlistId);
-}
-
-var playlistName;
-
 //Checks if valid playlistId
-async function checkPlaylistId(playlistId){
+var playlistName;
+async function checkPlaylistId(){
+    var playlistId = document.getElementById("playlistId").value;
     const playlistInfoURL = "https://youtube.googleapis.com/youtube/v3/playlists?key=" + youtubeKey + "&part=snippet%2Cstatus&id=";
     const playlistItemsURL = "https://youtube.googleapis.com/youtube/v3/playlistItems?key=" + youtubeKey + "&part=snippet&maxResults=50&playlistId=";
     var container = document.getElementById("invalid-id");
+
     const result = await fetch(playlistInfoURL + playlistId);
     result.json().then(data => {
         console.log("info: ", data);
         if (data.items.length == 0){
-            document.getElementById("current-videos").style.display = "none"; //Hide
+            document.getElementById("show-data").style.display = "none"; //Hide
             container.style.display = "block"; //Show error msg
         }
         else {
-            document.getElementById("current-videos").style.display = "block"; //Show
+            document.getElementById("show-data").style.display = "block"; //Show
             container.style.display = "none"; //Dont show error msg
             playlistName = data.items[0].snippet.localized.title;
+            document.getElementById("playlist-name").textContent = playlistName;
             callPlaylistItems(playlistItemsURL + playlistId);
         }
     });
 }
 
-//API Call, show data on website, save data into array to save
-var videoIds = {};
+//show videos in playlist, save data into json
+var saveThis = {};
 async function callPlaylistItems(URL){
     const result = await fetch(URL);
     result.json().then(data => {
@@ -59,61 +54,61 @@ async function callPlaylistItems(URL){
                 channelLink: channelLink,
                 videoLink: videoLink
             };
-            videoIds["video" + i] = childData;
+            saveThis["" + i] = childData;
 
             var container = document.getElementById("current-videos");
             var paragraph = document.createElement("p");
-            paragraph.innerHTML = "Added on " + added + ":<br>" + "<a href=" + videoLink + ">\"" + title + "\"</a>" + " by " + "<a href=" + channelLink + ">" + channel + "</a>";
+            paragraph.innerHTML = "Added on " + added + ":<br>" + "\"<a href=" + videoLink + ">" + title + "</a>\"" + " by " + "<a href=" + channelLink + ">" + channel + "</a>";
             container.appendChild(paragraph);
         }
     });
 }
 
+//empties currently shown videos (if any) so they dont keep adding
+function resetShownVideos(){
+    console.log("reset");
+    document.getElementById("current-videos").innerHTML = "";
+}
+
 window.addEventListener("load", () => {
-    document.getElementById("enter").onclick = playlistEntered;
+    document.getElementById("enter").addEventListener("click", () => {
+        console.log("pressed enter");
+        resetShownVideos();
+        checkPlaylistId();
+    });
     document.getElementById("save").addEventListener("click", () => {
-        saveData(videoIds);
+        console.log("pressed save");
+        saveData(playlistName, saveThis);
     });
     document.getElementById("load").addEventListener("click", () => {
-        loadData(videoIds);
+        console.log("pressed load");
+        loadData();
     });
 });
 
+async function loadData(){
+    var data = await getData();
+    console.log("loading: ", data);
 
-
-
-/*
-export function getData(){
     var container = document.getElementById("stored-data");
 
-    const databaseRef = ref(database, "/");
 
-    // Read data from the database
-    onValue(databaseRef, (snapshot) => {
-        const data = snapshot.val();
-        console.log("get: ", data);
+    var length = Object.keys(data).length;
+    for (var playlist = 0; playlist < length; playlist++) {
+        var name = Object.keys(data)[playlist];
 
-        for (const playlist in data) {
-            if (data.hasOwnProperty(playlist)) {
-                //naming the data
-                var title = data[playlist].title;
-                var channel = data[playlist].channel;
-                var id = data[playlist].id;
-                var added = data[playlist].added;
-                var channelLink = data[playlist].channelLink;
-                var videoLink = data[playlist].videoLink;
 
-                var paragraph = document.createElement("p");
-                //paragraph.innerHTML = "Added on " + added + ":<br>" + "<a href=" + videoLink + ">\"" + title + "\"</a>" + " by " + "<a href=" + channelLink + ">" + channel + "</a>";
-                container.appendChild(paragraph);
 
-                console.log(playlist);
-            }
-        }
-    });
+        //naming the data
+        var title = data.name.title;
+        var channel = data[playlist].channel;
+        var id = data[playlist].id;
+        var added = data[playlist].added;
+        var channelLink = data[playlist].channelLink;
+        var videoLink = data[playlist].videoLink;
+
+        var paragraph = document.createElement("p");
+        paragraph.innerHTML = "Added on " + added + ":<br>" + "<a href=" + videoLink + ">\"" + title + "\"</a>" + " by " + "<a href=" + channelLink + ">" + channel + "</a>";
+        container.appendChild(paragraph);
+    }
 }
-*/
-
-getData().then(data => {
-    console.log(data);
-});
